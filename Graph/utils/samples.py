eb7dac08ff30e3ba5a8fab1d51e87679d56ec062
George Gantus <georgegantus@usp.br>
2023-12-11 15:53:17 -0300

    add unconnected sample

--- Graph/utils/samples.py
+++ Graph/utils/samples.py
@@ -1,4 +1,6 @@
 from utils.edge import Edge
+
+#undirected as directed sample
 sample_graph = {
     0: [Edge(1,9),Edge(3,5),Edge(5,7),Edge(2,0)],
     1: [Edge(0,9),Edge(3,-2),Edge(6,4),Edge(4,3)],
@@ -7,4 +9,15 @@ sample_graph = {
     4: [Edge(1,3), Edge(6,6)],
     5: [Edge(2,6), Edge(0,7), Edge(3,2), Edge(6,1)],
     6: [Edge(5,1), Edge(3,3), Edge(1,4), Edge(4,6)],
+}
+
+
+unconnected_graph = {
+    0: [Edge(1,1)],
+    1: [Edge(6,1)],
+    2: [Edge(3,1)],
+    3: [Edge(5,1)],
+    4: [],
+    5: [Edge(4,1)],
+    6: [],
 }
